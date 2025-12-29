ec11cb69fdd64c82cf9f37555136d2dd6f1f5925
Sanjai451 <sanjaikumarr451@gmail.com>
2025-12-29 14:48:05 +0530

    updated with dockerization

--- src/main/java/com/learnToDeploy/DeployLearn/controller/Sample.java
+++ src/main/java/com/learnToDeploy/DeployLearn/controller/Sample.java
@@ -1,6 +1,7 @@
 package com.learnToDeploy.DeployLearn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Sample {
     @GetMapping("/hello")
     public String getHello(){
         return "Hello world";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String getHelloWithName(@PathVariable String name){
+        return "Hello world  " + Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 }
